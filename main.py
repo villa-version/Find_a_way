3d9a7cf7d41e5d6f1d049b50907ed18ee84efa58
villa <volinetsilia@gmail.com>
2021-10-26 14:14:06 +0300

    the program is almost work (it`s with errors)

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ CELL_NUMBER, CELL_SIZE = 20, 30
 
 class Cell:
 
-    def __init__(self, x, y, occupied, screen):
+    def __init__(self, x, y, occupied, screen, wrong):
         self.x = x
         self.y = y
         self.occupied = occupied
+        self.wrong = wrong
         self.screen = screen
 
     def draw(self):
@@ -31,11 +32,24 @@ class StartPoint:
         pygame.draw.rect(self.screen, (0, 255, 0), obj)
 
 
-class PathWay:
+class FinishPoint:
 
     def __init__(self, x, y, screen):
         self.x = x
         self.y = y
+        self.screen = screen
+
+    def draw(self):
+        obj = pygame.Rect(int(self.x * CELL_SIZE), int(self.y * CELL_SIZE), CELL_SIZE - 5, CELL_SIZE - 5)
+        pygame.draw.rect(self.screen, (255, 0, 255), obj)
+
+
+class PathWay:
+
+    def __init__(self, x, y, screen, direction):
+        self.x = x
+        self.y = y
+        self.direction = direction
         self.screen = screen
 
     def draw(self):
@@ -50,9 +64,13 @@ class MainController:
         self.grid = []
         self.spawn_grid()
         self.start_point = None
+        self.finish_point = None
         self.way = []
         self.direction = [0, 1, 2, 3]
         self.allow_to_spawn_first_pathway = True
+        self.way_out_of_a_trap_state = True
+        self.progress = 'SP'
+        self.last_dir = -1
 
     def update(self):
         self.draw_elem()
@@ -71,9 +89,18 @@ class MainController:
             pass
 
     def spawn_start_point(self, mx, my):
-        x = mx//CELL_SIZE
-        y = my//CELL_SIZE
-        self.start_point = StartPoint(x, y, self.screen)
+        if self.progress == 'SP':
+            self.progress = 'FP'
+            x = mx//CELL_SIZE
+            y = my//CELL_SIZE
+            self.start_point = StartPoint(x, y, self.screen)
+
+    def spawn_finish_point(self, mx, my):
+        if self.progress == 'FP':
+            self.progress = 'SP'
+            x = mx // CELL_SIZE
+            y = my // CELL_SIZE
+            self.finish_point = FinishPoint(x, y, self.screen)
 
     def spawn_grid(self):
         r = range(CELL_NUMBER)
@@ -81,39 +108,87 @@ class MainController:
             self.grid.append([])
         for y in r:
             for x in r:
-                self.grid[y].append(Cell(x, y, False, self.screen))
+                self.grid[y].append(Cell(x, y, False, self.screen, False))
 
-    def way_out_of_a_trap(self):
+    def way_out_of_a_trap(self, dir):
         counter = 0
-        if self.way[-1].y - 1 < 0:
-            counter += 1
-        else:
-            if self.grid[self.way[-1].y - 1][self.way[-1].x].occupied:
+        if self.way_out_of_a_trap_state:
+            if self.way[-1].y - 1 < 0:
                 counter += 1
-        if self.way[-1].y + 1 > CELL_NUMBER - 1:
-            counter += 1
-        else:
-            if self.grid[self.way[-1].y + 1][self.way[-1].x].occupied:
+            else:
+                if self.grid[self.way[-1].y - 1][self.way[-1].x].occupied:
+                    counter += 1
+            if self.way[-1].y + 1 > CELL_NUMBER - 1:
                 counter += 1
-        if self.way[-1].x - 1 < 0:
-            counter += 1
-        else:
-            if self.grid[self.way[-1].y][self.way[-1].x - 1].occupied:
+            else:
+                if self.grid[self.way[-1].y + 1][self.way[-1].x].occupied:
+                    counter += 1
+            if self.way[-1].x - 1 < 0:
                 counter += 1
-        if self.way[-1].x + 1 > CELL_NUMBER - 1:
-            counter += 1
-        else:
-            if self.grid[self.way[-1].y][self.way[-1].x + 1].occupied:
+            else:
+                if self.grid[self.way[-1].y][self.way[-1].x - 1].occupied:
+                    counter += 1
+            if self.way[-1].x + 1 > CELL_NUMBER - 1:
                 counter += 1
+            else:
+                if self.grid[self.way[-1].y][self.way[-1].x + 1].occupied:
+                    counter += 1
 
-        if counter == 4:
-            pass
+            self.last_dir = self.way[-1].direction
+
+            if counter == 4:
+                self.grid[self.way[-1].y][self.way[-1].x].occupied = False
+                self.grid[self.way[-1].y][self.way[-1].x].wrong = True
+                self.direction.remove(self.last_dir)
+                self.way.remove(self.way[-1])
+                self.way_out_of_a_trap_state = False
+        else:
+            if self.way[-1].y - 1 < 0:
+                counter += 1
+            else:
+                if self.grid[self.way[-1].y - 1][self.way[-1].x].occupied:
+                    counter += 1
+                elif self.grid[self.way[-1].y - 1][self.way[-1].x].wrong:
+                    counter += 1
+            if self.way[-1].y + 1 < CELL_NUMBER - 1:
+                counter += 1
+            else:
+                if self.grid[self.way[-1].y + 1][self.way[-1].x].occupied:
+                    counter += 1
+                elif self.grid[self.way[-1].y + 1][self.way[-1].x].wrong:
+                    counter += 1
+            if self.way[-1].x - 1 < 0:
+                counter += 1
+            else:
+                if self.grid[self.way[-1].y][self.way[-1].x - 1].occupied:
+                    counter += 1
+                elif self.grid[self.way[-1].y][self.way[-1].x - 1].wrong:
+                    counter += 1
+            if self.way[-1].x + 1 < CELL_NUMBER - 1:
+                counter += 1
+            else:
+                if self.grid[self.way[-1].y][self.way[-1].x + 1].occupied:
+                    counter += 1
+                elif self.grid[self.way[-1].y][self.way[-1].x + 1].wrong:
+                    counter += 1
+
+            if counter < 4:
+                self.way_out_of_a_trap_state = True
+                self.last_dir = -1
+            else:
+                self.grid[self.way[-1].y][self.way[-1].x].wrong = True
+                self.grid[self.way[-1].y][self.way[-1].x].occupied = False
+                self.way.remove(self.way[-1])
+                if self.way[-1].direction != self.last_dir:
+                    self.direction.remove(self.last_dir)
+                else:
+                    self.last_dir = self.way[-1].direction
 
     def choose_dir(self):
         new_direction = rand(self.direction[0], self.direction[-1])
         if self.allow_to_spawn_first_pathway:
             try:
-                self.way.append(PathWay(self.start_point.x, self.start_point.y + 1, self.screen))
+                self.way.append(PathWay(self.start_point.x, self.start_point.y + 1, self.screen, new_direction))
                 self.grid[self.start_point.y + 1][self.start_point.x].occupied = True
                 self.grid[self.start_point.y][self.start_point.x].occupied = True
                 self.allow_to_spawn_first_pathway = False
@@ -125,7 +200,7 @@ class MainController:
                     self.direction = [2, 3]
                 else:
                     if not self.grid[self.way[-1].y - 1][self.way[-1].x].occupied:
-                        self.way.append(PathWay(self.way[-1].x, self.way[-1].y - 1, self.screen))
+                        self.way.append(PathWay(self.way[-1].x, self.way[-1].y - 1, self.screen, new_direction))
                         self.grid[self.way[-1].y][self.way[-1].x].occupied = True
                         self.direction = [0, 2, 3]
                     else:
@@ -135,7 +210,7 @@ class MainController:
                     self.direction = [2, 3]
                 else:
                     if not self.grid[self.way[-1].y + 1][self.way[-1].x].occupied:
-                        self.way.append(PathWay(self.way[-1].x, self.way[-1].y + 1, self.screen))
+                        self.way.append(PathWay(self.way[-1].x, self.way[-1].y + 1, self.screen, new_direction))
                         self.grid[self.way[-1].y][self.way[-1].x].occupied = True
                         self.direction = [1, 2, 3]
                     else:
@@ -145,7 +220,7 @@ class MainController:
                     self.direction = [0, 1]
                 else:
                     if not self.grid[self.way[-1].y][self.way[-1].x - 1].occupied:
-                        self.way.append(PathWay(self.way[-1].x - 1, self.way[-1].y, self.screen))
+                        self.way.append(PathWay(self.way[-1].x - 1, self.way[-1].y, self.screen, new_direction))
                         self.grid[self.way[-1].y][self.way[-1].x].occupied = True
                         self.direction = [0, 1, 2]
                     else:
@@ -155,12 +230,12 @@ class MainController:
                     self.direction = [0, 1]
                 else:
                     if not self.grid[self.way[-1].y][self.way[-1].x + 1].occupied:
-                        self.way.append(PathWay(self.way[-1].x + 1, self.way[-1].y, self.screen))
+                        self.way.append(PathWay(self.way[-1].x + 1, self.way[-1].y, self.screen, new_direction))
                         self.grid[self.way[-1].y][self.way[-1].x].occupied = True
                         self.direction = [0, 1, 3]
                     else:
                         self.direction = [0, 1, 2]
-            self.way_out_of_a_trap()
+            self.way_out_of_a_trap(new_direction)
 
 
 def main():
@@ -176,6 +251,7 @@ def main():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 main_controller.spawn_start_point(mouse_x, mouse_y)
+                main_controller.spawn_finish_point(mouse_x, mouse_y)
 
         screen.fill((255, 255, 255))
         main_controller.update()
